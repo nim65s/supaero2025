51f8878223819e87cbc3394ea3d118d94a15e3a1
Mansard <nmansard@laas.fr>
2024-02-27 21:45:25 +0100

    [tp4] monkey patch of computeDistances and computeCollisions.

--- tp4/compatibility.py
+++ tp4/compatibility.py
@@ -7,25 +7,88 @@ and pinocchio3x/hppfcl3x (version 2.99 and future).
 
 import pinocchio as pin
 import hppfcl
+import numpy as np
 
 P3X = pin.__version__.split('.')[1] == '99'
 HPPFCL3X = hppfcl.__version__.split('.')[1] == '99'
 
-if HPPFCL3X:
-    def hppfcl_normals(n):
+# -------------------------------------------------------------------------------
+if not P3X:
+    pin.ZAxis = np.array([0,0,1.])
+
+# -------------------------------------------------------------------------------
+# Monkey patch of computeDistances and computeCollisions to mimic p3x behavior
+
+if not HPPFCL3X:
+    pin._computeDistances = pin.computeDistances
+    pin._computeCollisions = pin.computeCollisions
+
+    def computeDistances(model,data,geometry_model,geometry_data,q):
         '''
-        This functions is introduced for compatibility with earlier versions of 
-        HPP-FCL. With versions 3x, this function is the identity (return the input).
+        Mimic the behavior of computeDistances in pinocchio3x, by reversing the normals.
         '''
-        return n
-else:
-    def hppfcl_normals(n):
+        pin._computeDistances(model,data,geometry_model,geometry_data,q)
+        for d in geometry_data.distanceResults:
+            d.normal *= -1
+
+    def computeCollisions(model,data,geometry_model,geometry_data,q,stop_at_first_collision=False):
         '''
-        This functions is introduced for compatibility with earlier versions of 
-        HPP-FCL. 
-        The version returns the opposite of the input normal vector.
-        In version HPP-FCL 2x, the normal was defined in the wrong direction (i.e from
-        point P2 to P1). In the 3x version, it is the opposite. We align with the
-        P3X convention.
+        Mimic the behavior of pin.computeCollisions by relying on computeDistances
+        which is more generic in p2x.
+        BIG LIMITATIONS: only one single contact point can be detected
         '''
-        return -n
+        computeDistances(model,data,geometry_model,geometry_data,q)
+        for p,c,d in zip(geometry_model.collisionPairs,geometry_data.collisionResults,geometry_data.distanceResults):
+            c.clear()
+            id1,id2 = p.first,p.second
+            g1,g2 = geometry_model.geometryObjects[id1],geometry_model.geometryObjects[id2]
+            contact = hppfcl.Contact(g1.geometry,g2.geometry,
+                                     d.b1,d.b2,
+                                     (d.getNearestPoint1()+d.getNearestPoint2())/2,
+                                     d.normal,
+                                     np.dot(d.normal,d.getNearestPoint2()-d.getNearestPoint1()))
+            c.addContact(contact)
+
+    computeDistances.__doc__ += '\n\nOriginal doc:\n' + pin._computeDistances.__doc__
+    computeCollisions.__doc__ += '\n\nOriginal doc:\n' + pin._computeCollisions.__doc__
+    pin.computeDistances = computeDistances
+    pin.computeCollisions = computeCollisions
+
+# -------------------------------------------------------------------------------
+if not P3X:
+
+    from enum import Enum
+    class ContactType(Enum):
+        CONTACT_3D = 3
+        CONTACT_6D = 6
+
+    class RigidConstraintData:
+        pass
+
+    class RigidConstraintModel:
+        def __init__(self,
+                     contactType,
+                     model: pin.Model,
+                     joint1_id,
+                     joint1_placement,
+                     joint2_id,
+                     joint2_placement,
+                     referenceFrame):
+            assert(referenceFrame in pin.ReferenceFrame.values)
+            assert(contactType in [ ContactType.CONTACT_3D, ContactType.CONTACT_6D ])
+
+            self.contactType = contactType
+            self.joint1_id = joint1_id
+            self.joint1_placement = joint1_placement
+            self.joint2_id = joint2_id
+            self.joint2_placement = joint2_placement
+            self.referenceFrame = referenceFrame
+
+            self.name = ""
+
+        def createData(self):
+            return RigidConstraintData()
+
+    pin.ContactType = ContactType
+    pin.RigidConstraintModel = RigidConstraintModel
+    pin.RigidConstraintData = RigidConstraintData
